9931eeb61588700b38997657c8b6ace087f4afb1
m00219907 <747959177@qq.com>
2020-06-30 16:45:39 +0800

    add D8 proguard

--- proguard.py
+++ proguard.py
@@ -3,14 +3,15 @@ import random
 # D1 Gσ6C
 # D2 ρβpP
 # D3 μυuU
+# D8 αβγδεζηθικλμνξοπρστυφχψωΓΔΘΛΞΟΠΣΦΨΩ
 
 outfile = open("dict.txt", "w", encoding='utf-8')
 resultList=[]
 for i in range(50000):
-    count = random.randint(2,7)
+    count = random.randint(1,3)#D8(1,3) other(2,7)
     result=''
     for m in range(count):
-        result += random.choice('μυuU')
+        result += random.choice('αβγδεζηθικλμνξοπρστυφχψωΓΔΘΛΞΟΠΣΦΨΩ')
     if result not in resultList:
         resultList.append(result)
         outfile.write(result)
